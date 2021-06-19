d53cf7dabd7f09dec25ab35650894a7c0d1012f7
Dynamic-Code <dynamic2106@gmail.com>
2021-06-19 23:36:42 +0530

    END of sec 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model:any = {}
+  @Output() cancelRegister = new EventEmitter();
+  constructor(private accountService:AccountService) { }
+
+  ngOnInit(): void {
+  }
+  register(){
+    console.log(this.model);
+    this.accountService.register(this.model).subscribe(res => {
+      console.log(res);
+      this.cancel();
+    }, error =>{
+      console.log(error);
+    })
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+}
